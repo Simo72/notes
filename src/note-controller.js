7c54d27eaeadd483d5fa5faba3ce9e1fcf13d680
Kynosaur <kye.bracey@gmail.com>
2017-06-21 15:40:30 +0100

    Adds basic NoteController object

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,3 +1,12 @@
 // element = document.getElementById("app");
 //
 // element.innerHTML = "Howdy"
+
+(function(exports) {
+
+  function NoteController(noteList) {
+    this.noteList = noteList;
+  };
+
+  exports.NoteController = NoteController;
+})(this);
--- tests/note-controller-test.js
+++ tests/note-controller-test.js
@@ -1,7 +1,9 @@
 function testThatNoteControllerCanBeInstantiated() {
   var noteList = new NoteList();
   var noteController = new NoteController(noteList);
-  assert.isTrue(noteController.noteList === noteList);
+  if(noteController.noteList !== noteList) {
+    throw new Error("noteController.noteList does not contain a note list object")
+  }
 };
 
 testThatNoteControllerCanBeInstantiated();
